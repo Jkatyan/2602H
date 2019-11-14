4ec46a3cdda0a81b44ddba0c0bcf093e03f96858
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2019-11-13 21:17:14 -0500

    Commit 1) 11/13/19

---
+++ src/main.cpp
@@ -0,0 +1,191 @@
+#include "main.h"
+#include "odometry.hpp"
+#include "PID.hpp"
+
+pros::ADIGyro gyro ('b', 1);
+
+const int LDPORT = 1; //Make these negative to rev port
+const int LD2PORT = 2;
+const int RDPORT = 3;
+const int RD2PORT = 4;
+
+const int RIGHTLIFTPORT = 5;
+const int LEFTLIFTPORT = 8;
+const int CHASSISPORT = 6;
+const int HOOKPORT = 7;
+
+const int RC = 1; //Chassis Speed Correction
+const int LC = 1;
+
+PID drivePID;
+PID turnPID;
+
+float lastSlewTime;
+float maxAccel = 0.14;
+float lastSlewRate = 0;
+
+void goTo(float targetX, float targetY);
+void setDrive(int left, int right);
+void rotate(int degrees, int voltage);
+float slewRateCalculate(float desiredRate);
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::Motor LD(LDPORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor LD2(LD2PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor RD(RDPORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor RD2(RD2PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor RIGHTLIFT(RIGHTLIFTPORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor LEFTLIFT(LEFTLIFTPORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor CHASSISEXTENSION(CHASSISPORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor INTAKE(HOOKPORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::ADIEncoder sideEnc('G', 'H');
+
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
+
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+	pros::lcd::register_btn1_cb(on_center_button);
+	pros::ADIGyro gyro ('b', 1);
+	pros::delay(2000);
+}
+void disabled() {}
+void competition_initialize() {}
+
+void autonomous() {
+
+	goTo(0, 10);
+	rotate(180, 127);
+	goTo(0,0);
+
+}
+void opcontrol() {
+	 // pros::lcd::initialize();
+	 // pros::lcd::set_text(1, "Hello World");
+	drivePID = pidInit (7, 0, 0, 0, 100.0,5,15);
+	turnPID = pidInit(40, 0, 0, 0, 100.0,5,15);
+	lastSlewTime = pros::millis();
+
+	while (true) {
+		LD.move(LC*(controller.get_analog(ANALOG_LEFT_Y)));
+		LD2.move(LC*(controller.get_analog(ANALOG_LEFT_Y)));
+		RD.move(RC*(controller.get_analog(ANALOG_RIGHT_Y)));
+		RD2.move(RC*(controller.get_analog(ANALOG_RIGHT_Y)));
+
+		/*if (controller.get_digital_new_press(DIGITAL_X)) {
+			moveToPoint(0, 0);
+		}*/
+		updatePosition();
+
+		pros::lcd::print(0, "X: %f", getX());
+		pros::lcd::print(1, "Y: %f", getY());
+		pros::lcd::print(2, "Angle: %f", getAngleDegrees());
+
+		pros::lcd::print(4, "Side Encoder: %d", sideEnc.get_value());
+		pros::lcd::print(5, "Left Encoder: %f", LD.get_position());
+		pros::lcd::print(6, "Right Encoder: %f", RD.get_position());
+
+		pros::delay(10);
+	}
+}
+
+void goTo(float targetX, float targetY) {
+	bool atPoint = false;
+	float targetAngle =0;
+	float power =0;
+	float turnPower =0;
+	lastSlewTime = pros::millis();
+
+	while (!atPoint) {
+		updatePosition();
+
+		power = -pidCalculate(drivePID, 0, sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)));
+		power = slewRateCalculate(power);
+
+		targetAngle = atan2f((targetY-getY()),(targetX-getX()))-M_PI/2;
+		if (targetAngle >= M_PI) {
+	    targetAngle-=2*M_PI;
+	  }
+	  else if (targetAngle <= -M_PI) {
+	    targetAngle+=2*M_PI;
+	  }
+
+		turnPower = ((fabs(targetAngle-getAngle())>M_PI)? -1: 1)*pidCalculate(turnPID, targetAngle, getAngle());
+
+		LD.move((power + turnPower)*LC);
+		RD.move((power - turnPower)*RC);
+		LD2.move((power + turnPower)*LC);
+		RD2.move((power - turnPower)*RC);
+
+		pros::lcd::print(0, "X: %f", getX());
+		pros::lcd::print(1, "Y: %f", getY());
+		pros::lcd::print(2, "Angle: %f", getAngleDegrees());
+
+		pros::lcd::print(7, "Target Angle: %f", targetAngle*180/M_PI);
+
+
+		if (sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)) < 3 || controller.get_digital(DIGITAL_B)) {
+			atPoint = true;
+		}
+		pros::delay(10);
+	}
+}
+
+float slewRateCalculate (float desiredRate) {
+		//pros::lcd::print(7, "called: %f", desiredRate);
+		float deltaTime = pros::millis()-lastSlewTime;
+		float desiredAccel = (desiredRate -lastSlewRate)/deltaTime;
+		float addedRate;
+		float newRate;
+
+		if (fabs(desiredAccel) < maxAccel || (desiredAccel<0 && desiredRate>0) || (desiredAccel>0 && desiredRate<0)) {
+		    addedRate = desiredAccel*deltaTime;
+		    newRate = addedRate+lastSlewRate;
+		}
+		else {
+		    addedRate = ((desiredAccel>0)? 1: -1)*maxAccel*deltaTime;
+        newRate = addedRate+lastSlewRate;
+		}
+	  lastSlewTime = lastSlewTime+deltaTime;
+	  lastSlewRate = newRate;
+
+		float returnVal = newRate;
+		return returnVal;
+}
+
+void setDrive(int left, int right){
+	LD = left;
+	LD2 = left;
+	RD = right;
+	RD2 = right;
+}
+
+void rotate(int degrees, int voltage){
+	int direction = abs(degrees) / degrees;
+	gyro.reset();
+	setDrive(-voltage * direction, voltage * direction);
+	while(fabs(gyro.get_value()) < abs(degrees * 10) - 50){
+		pros::delay(10);
+	}
+	pros::delay(100);
+	if(fabs(gyro.get_value()) > abs(degrees * 10)){
+		setDrive(0.5 * voltage * direction, 0.5 * -voltage * direction);
+		while(fabs(gyro.get_value()) > abs(degrees * 10)){
+			pros::delay(10);
+		}
+	}
+	else if(fabs(gyro.get_value()) < abs(degrees * 10)){
+		setDrive(0.5 * -voltage * direction, 0.5 * voltage * direction);
+		while(fabs(gyro.get_value()) > abs(degrees * 10)){
+			pros::delay(10);
+		}
+	}
+	setDrive(0,0);
+}
---
+++ src/odometry.cpp
@@ -0,0 +1,166 @@
+#ifndef ODOMETRY_CPP
+#define ODOMETRY_CPP
+
+#include "odometry.hpp"
+
+/* PILONS Code:
+void trackPosition(int left, int right, int back, sPos& position)
+{
+	float L = (left - position.leftLst) * SPIN_TO_IN_LR; // The amount the left side of the robot moved
+	float R = (right - position.rightLst) * SPIN_TO_IN_LR; // The amount the right side of the robot moved
+	float S = (back - position.backLst) * SPIN_TO_IN_S; // The amount the back side of the robot moved
+
+	// Update the last values
+	position.leftLst = left;
+	position.rightLst = right;
+	position.backLst = back;
+
+	float h; // The hypotenuse of the triangle formed by the middle of the robot on the starting position and ending position and the middle of the circle it travels around
+	float i; // Half on the angle that I've traveled
+	float h2; // The same as h but using the back instead of the side wheels
+	float a = (L - R) / (L_DISTANCE_IN + R_DISTANCE_IN); // The angle that I've traveled
+	if (a)
+	{
+		float r = R / a; // The radius of the circle the robot travel's around with the right side of the robot
+		i = a / 2.0;
+		float sinI = sin(i);
+		h = ((r + R_DISTANCE_IN) * sinI) * 2.0;
+
+		float r2 = S / a; // The radius of the circle the robot travel's around with the back of the robot
+		h2 = ((r2 + S_DISTANCE_IN) * sinI) * 2.0;
+	}
+	else
+	{
+		h = R;
+		i = 0;
+
+		h2 = S;
+	}
+	float p = i + position.a; // The global ending angle of the robot
+	float cosP = cos(p);
+	float sinP = sin(p);
+
+	// Update the global position
+	position.y += h * cosP;
+	position.x += h * sinP;
+
+	position.y += h2 * -sinP; // -sin(x) = sin(-x)
+	position.x += h2 * cosP; // cos(x) = cos(-x)
+
+	position.a += a;
+}
+*/
+
+float Sl = 5.125; //distance from tracking center to middle of left wheel
+float Sr = 5.125; //distance from tracking center to middle of right wheel
+float Ss = 7.75; //distance from tracking center to middle of the tracking wheel
+float wheelDiameter = 4.125; //diameter of the side wheels being used for tracking
+float trackingDiameter = 4.125; //diameter of the sideways tracking wheel
+
+float x = 0;
+float y = 0;
+float angle = 0;
+
+float lastLeftPos = 0;
+float lastRightPos = 0;
+float lastSidePos = 0;
+
+float deltaTheta = 0;
+float thetaNew = 0;
+float thetaM = 0;
+
+float curLeft = 0;
+float curRight = 0;
+float curSide = 0;
+
+float leftAtReset = 0;
+float rightAtReset = 0;
+float thetaReset = 0;
+
+float deltaLeft = 0;
+float deltaRight = 0;
+float deltaSide = 0;
+
+float deltaLr = 0;
+float deltaRr = 0;
+
+float deltaX = 0;
+float deltaY = 0;
+
+float theta = 0;
+float radius = 0;
+
+void updatePosition() {
+  curLeft = LD.get_position();
+  curRight = RD.get_position();
+  curSide = sideEnc.get_value();
+
+  deltaLeft = (curLeft - lastLeftPos)*(M_PI/180)*(wheelDiameter/2);
+  deltaRight = (curRight - lastRightPos)*(M_PI/180)*(wheelDiameter/2);
+  deltaSide = (curSide - lastSidePos)*(M_PI/180)*(trackingDiameter/2);
+
+  lastLeftPos = curLeft;
+  lastRightPos = curRight;
+  lastSidePos = curSide;
+
+  deltaLr = (curLeft - leftAtReset)*(M_PI/180)*(wheelDiameter/2);
+  deltaRr = (curRight - rightAtReset)*(M_PI/180)*(wheelDiameter/2);
+
+  thetaNew = (thetaReset + (deltaLr - deltaRr)/(Sl + Sr));
+
+  deltaTheta = thetaNew - angle;
+
+  deltaSide = deltaSide-Ss*deltaTheta;
+
+  if (deltaTheta == 0) {
+    deltaX = deltaSide;
+    deltaY = deltaRight;
+  }
+  else {
+    deltaX = (2*sin(deltaTheta/2))*(deltaSide/deltaTheta + Ss);
+    deltaY = (2*sin(deltaTheta/2))*(deltaRight/deltaTheta +Sr);
+  }
+
+  thetaM = angle + deltaTheta/2;
+
+  theta = atan2f(deltaY, deltaX);
+  radius = sqrt(deltaX*deltaX + deltaY*deltaY);
+  theta = theta-thetaM;
+  deltaX = radius*cos(theta);
+  deltaY = radius*sin(theta);
+
+  thetaNew+=M_PI;
+  while (thetaNew <= 0) {
+    thetaNew+=2*M_PI;
+  }
+  thetaNew = modulo(thetaNew, 2*M_PI);
+  thetaNew-=M_PI;
+
+  angle = thetaNew;
+  x = x - deltaX;
+  y = y + deltaY;
+}
+
+float getX() {
+  return x;
+}
+
+float getY() {
+  return y;
+}
+
+float getAngleDegrees() {
+  return angle*180/M_PI;
+}
+float getAngle() {
+  return angle;
+}
+
+float modulo(float a, float b) {
+  while (a>b) {
+    a-=b;
+  }
+  return a;
+}
+
+#endif
