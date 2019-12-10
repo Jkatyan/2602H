42b9911fb84dcea4ad2232415311d46106d20ab0
Jay Katyan <33378744+Jkatyan@users.noreply.github.com>
2019-12-10 15:50:14 -0500

    Added advanced auton functions in autonLib. Fixed declarations.

--- src/autonLib/autonLib.cpp
+++ src/autonLib/autonLib.cpp
@@ -1,8 +1,132 @@
 #include "autons.hpp"
 
-PID drivePID;
-PID turnPID;
+extern PID drivePID;
+extern PID turnPID;
 
-float lastSlewTime;
-float maxAccel = 0.14;
-float lastSlewRate = 0;
+extern float lastSlewTime;
+extern float maxAccel;
+extern float lastSlewRate;
+
+void goTo(float targetX, float targetY) {
+	bool atPoint = false;
+	float targetAngle =0;
+	float power =0;
+	float turnPower =0;
+	lastSlewTime = pros::millis();
+
+	while (!atPoint) {
+		updatePosition();
+
+		power = -pidCalculate(drivePID, 0, sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)));
+		power = slewRateCalculate(power);
+
+		targetAngle = atan2f((targetY-getY()),(targetX-getX()))-M_PI/2;
+		if (targetAngle >= M_PI) {
+	    targetAngle-=2*M_PI;
+	  }
+	  else if (targetAngle <= -M_PI) {
+	    targetAngle+=2*M_PI;
+	  }
+
+		turnPower = ((fabs(targetAngle-getAngle())>M_PI)? -1: 1)*pidCalculate(turnPID, targetAngle, getAngle());
+
+		LD.move((power + turnPower)*LC);
+    RD.move((power - turnPower)*RC);
+		LD2.move((power + turnPower)*LC);
+		RD2.move((power - turnPower)*RC);
+
+		pros::lcd::print(0, "X: %f", getX());
+		pros::lcd::print(1, "Y: %f", getY());
+		pros::lcd::print(2, "Angle: %f", getAngleDegrees());
+
+		pros::lcd::print(7, "Target Angle: %f", targetAngle*180/M_PI);
+
+
+		if (sqrt(pow(targetY-getY(),2) + pow(targetX-getX(),2)) < 3 || controller.get_digital(DIGITAL_B)) {
+			atPoint = true;
+		}
+		pros::delay(10);
+	}
+}
+
+float slewRateCalculate (float desiredRate) {
+		//pros::lcd::print(7, "called: %f", desiredRate);
+		float deltaTime = pros::millis()-lastSlewTime;
+		float desiredAccel = (desiredRate -lastSlewRate)/deltaTime;
+		float addedRate;
+		float newRate;
+
+		if (fabs(desiredAccel) < maxAccel || (desiredAccel<0 && desiredRate>0) || (desiredAccel>0 && desiredRate<0)) {
+		    addedRate = desiredAccel*deltaTime;
+		    newRate = addedRate+lastSlewRate;
+		}
+		else {
+		    addedRate = ((desiredAccel>0)? 1: -1)*maxAccel*deltaTime;
+        newRate = addedRate+lastSlewRate;
+		}
+	  lastSlewTime = lastSlewTime+deltaTime;
+	  lastSlewRate = newRate;
+
+		float returnVal = newRate;
+		return returnVal;
+}
+
+void setDrive(int left, int right){
+	LD = left;
+	LD2 = left;
+	RD = right;
+	RD2 = right;
+}
+
+void rotate(int degrees, int voltage){
+	int direction = abs(degrees) / degrees;
+	//gyro.reset();
+	setDrive(-voltage * direction, voltage * direction);
+	while(fabs(gyro.get_value()) < abs(degrees * 10) - 50){
+		pros::delay(10);
+	}
+	pros::delay(100);
+	if(fabs(gyro.get_value()) > abs(degrees * 10)){
+		setDrive(0.5 * voltage * direction, 0.5 * -voltage * direction);
+		while(fabs(gyro.get_value()) > abs(degrees * 10)){
+			pros::delay(10);
+		}
+	}
+	else if(fabs(gyro.get_value()) < abs(degrees * 10)){
+		setDrive(0.5 * -voltage * direction, 0.5 * voltage * direction);
+		while(fabs(gyro.get_value()) > abs(degrees * 10)){
+			pros::delay(10);
+		}
+	}
+	setDrive(0,0);
+}
+
+void driveTarget(int target, int time, float speed){
+  int atTarget = 0;
+  int driveEnc = 0;
+  int distance = 0;
+  int startTime = pros::millis();
+
+    while ((atTarget != 1) && (pros::millis()-startTime) < time) {
+    driveEnc = ((abs(leftEnc.get_value()))+(abs(rightEnc.get_value())))/2;
+    distance = target - driveEnc;
+
+		pros::lcd::print(1, "Drive Encoder Value: %f", driveEnc);
+		pros::lcd::print(2, "Distance from Target: %f", distance);
+
+    float val = pidCalculate(drivePID, target, driveEnc)*speed;
+    val = slewRateCalculate(val);
+    int rightVal = val;
+    int leftVal = val;
+
+    RD.move(RC*rightVal);
+    RD2.move(RC*rightVal);
+    LD.move(LC*leftVal);
+    LD2.move(LC*leftVal);
+    if(driveEnc == target){
+       atTarget = 1;
+       pros::lcd::print(2, "Distance from Target: %f", distance);
+      }
+      pros::delay(20);
+    }
+}
