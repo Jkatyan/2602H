89ee113a2dbe88150001b176c712ea26bcc36855
AAA_shuibiao <AAA_shuibiao@163.com>
2020-01-27 11:45:19 -0500

    Added meme.py

--- configApply.py
+++
@@ -1,9 +0,0 @@
-#import sys
-import os
-
-#config_json_file = open("config.json", 'r')
-#config_cpp_file = open("src\\config.py_generated.cpp")
-
-#sys.stdout = config_cpp_file
-
-os.system('start')
